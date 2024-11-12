60bb630a7253b617d37800f805bd892374acb774
rlawogns123 <evolui@naver.com>
2024-11-12 14:05:03 +0900

    [FIX] : 전역 폰트 재설정 및 public 이미지 제거 KAN-102

--- apps/main-web/src/components/pages/profile/PetList.tsx
+++ apps/main-web/src/components/pages/profile/PetList.tsx
@@ -1,13 +1,13 @@
-import ProfileAvatar from "../../ui/ProfileAvatar";
-import React from "react";
+import ProfileAvatar from '../../ui/ProfileAvatar';
+import React from 'react';
 
 const petList = [
-  { id: 0, name: "전체", imgUrl: "" },
-  { id: 1, name: "신지훈1", imgUrl: "/jihoon3.png" },
-  { id: 2, name: "신지훈2", imgUrl: "/jihoon.jpg" },
-  { id: 3, name: "신지훈3", imgUrl: "/jihunpistol.jpg" },
-  { id: 4, name: "신지훈4", imgUrl: "/jihoon4.jpg" },
-  { id: 5, name: "박상언1", imgUrl: "/sangeun.jpg" },
+  { id: 0, name: '전체', imgUrl: '' },
+  { id: 1, name: '신지훈1', imgUrl: '/jihunpistol.jpg' },
+  { id: 2, name: '신지훈2', imgUrl: '/jihunpistol.jpg' },
+  { id: 3, name: '신지훈3', imgUrl: '/jihunpistol.jpg' },
+  { id: 4, name: '신지훈4', imgUrl: '/jihunpistol.jpg' },
+  { id: 5, name: '박상언1', imgUrl: '/jihunpistol.jpg' },
 ];
 
 function PetList() {
@@ -18,8 +18,8 @@ function PetList() {
           <li key={item.id}>
             <ProfileAvatar
               imgUrl={item.imgUrl}
-              w={"12"}
-              h={"12"}
+              w={'12'}
+              h={'12'}
               name={item.name}
             />
           </li>
