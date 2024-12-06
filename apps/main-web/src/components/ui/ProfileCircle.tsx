8ecf6653364506542e051614fb9fdff7891953b2
storyoser <storyoser@pukyong.ac.kr>
2024-12-06 10:18:02 +0900

    [FEAT] : Add Feed 부분 오른쪽 상단 헤더 프로필 이미지 fetch로 변경 KAN-407

--- apps/main-web/src/components/ui/ProfileCircle.tsx
+++ apps/main-web/src/components/ui/ProfileCircle.tsx
@@ -1,12 +1,25 @@
 import Image from 'next/image';
-export interface PetProfile {
-  name: string;
-  profileImage: string;
-}
+import { useEffect, useState } from 'react';
+import { getPetList } from '../../actions/user';
+import { useSession } from '../../context/SessionContext';
+import { PetInfo } from '../../types/user';
 
 export default function ProfileCircle() {
   // 여기서 userUuid로 Pet 프로필 이미지를 가져오는 로직을 작성합니다.
-
+  const session = useSession();
+  const [petProfile, setPetProfile] = useState<PetInfo[]>([]);
+  useEffect(() => {
+    const uuid = session?.uuid;
+    if (!uuid) {
+      console.error('UUID is missing.');
+      return;
+    }
+    const fetchpetProfile = async () => {
+      const data = await getPetList(uuid);
+      console.log(data);
+    };
+    fetchpetProfile();
+  });
   return (
     <div className="mx-auto w-full max-w-screen-sm p-4">
       <div className="scrollbar-hide flex gap-4 overflow-x-auto pb-4">
