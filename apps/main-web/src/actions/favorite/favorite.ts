42d2cad9e660f19f543e85b0e1a7faf992e8a35e
storyoser <storyoser@pukyong.ac.kr>
2024-12-09 11:02:39 +0900

    [FEAT] : 피드 상세페이지에서 좋아요 기능과 공유기능 구현 KAN-438

--- apps/main-web/src/actions/favorite/favorite.ts
+++ apps/main-web/src/actions/favorite/favorite.ts
@@ -1,3 +1,4 @@
+'use server';
 import { revalidateTag } from 'next/cache';
 import { CommonResponse, responseList } from '../../types/responseType';
 import { fetchDataforMembers } from '../common/common';
