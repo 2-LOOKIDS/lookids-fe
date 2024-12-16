e2016e92079e98da65d61835d13e6574742f6334
storyoser <storyoser@pukyong.ac.kr>
2024-12-16 10:42:23 +0900

    [FEAT] : 메인페이지에서 팔로우한 유저의 피드 이후 랜덤 피드 노출 KAN-548

--- apps/main-web/src/components/ui/CommonHeader.tsx
+++ apps/main-web/src/components/ui/CommonHeader.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ChevronLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import { CommonHeaderProps } from '../../types/common/MenuType';
 import CommonMenu from '../common/CommonMenu';
 
@@ -9,12 +10,15 @@ export default function CommonHeader({
   ismenu,
   menuItems = [],
 }: CommonHeaderProps) {
+  const router = useRouter();
   return (
     // mt-[52px]
     <section className="relative flex h-12 items-center">
       <ChevronLeft
         className="absolute left-3 cursor-pointer"
-        onClick={() => (window.location.href = '/')}
+        onClick={() => {
+          router.back();
+        }}
       />
       {/* 메뉴 버튼 */}
       {ismenu && menuItems.length > 0 && (
